df27b5dd67dc4c60e51cc82cf35321718002e7c3
Spencer Haugh <spencer.haugh@gmail.com>
2025-10-29 08:45:55 -0400

    dev tools update

--- app/components/NavbarComponent/NavbarComponent.tsx
+++ app/components/NavbarComponent/NavbarComponent.tsx
@@ -3,11 +3,15 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import styles from "./NavbarComponent.module.scss"
-import { RouteLink } from "@/app/app.config";
+import { logToDevTools, RouteLink } from '@/app/app.config';
+import { useEffect } from 'react';
 
 const NavbarComponent = ({links}: { links: RouteLink[]}) => {
     const pathname = usePathname();
-
+    const preventRelog = null;
+    
+    useEffect(() => logToDevTools(), [preventRelog]);
+    
     return (
         <div className={styles.host}>
             <nav className={styles.navbar}>
